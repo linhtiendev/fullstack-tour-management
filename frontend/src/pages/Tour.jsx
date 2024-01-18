1f50edb1704cfcfe8ba4eeca3c27bef0389baaec
linhtiendev <lelinhtien0707@gmail.com>
2024-01-18 23:49:31 +0700

    feat: design UI header

---
+++ frontend/src/pages/Tour.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <div>
+      Tour
+    </div>
+  )
+}
+
+export default Tours
---
+++ frontend/src/pages/TourDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TourDetails = () => {
+  return (
+    <div>
+        TourDetail
+    </div>
+  )
+}
+
+export default TourDetails

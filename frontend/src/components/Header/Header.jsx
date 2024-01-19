c33e62e9b20d303717de59cc3c13531b02b444c6
linhtiendev <lelinhtien0707@gmail.com>
2024-01-19 16:15:25 +0700

    fix: icon menu header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
               </div>
               {/*========= mobile menu =========*/}
               <span className="mobile__menu">
-                <i class="ri-menu-line"></i>
+                <i className="ri-menu-line"></i>
               </span>
             </div>
           </div>

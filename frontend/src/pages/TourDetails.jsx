8b02eb12cf2545fe2b86d857de0b000c64ec4268
linhtiendev <lelinhtien0707@gmail.com>
2024-01-22 23:00:44 +0700

    feat: design tour detail UI

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
+import '../styles/tour-details.css'
+
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
+import { useParams } from 'react-router-dom'
+import tourData from '../assets/data/tours'
+
+import calculateRating from '../utils/avgRating'
 
 const TourDetails = () => {
+
+  const {id} = useParams()
+
+  // This is an static data later we will call our API and load our data from db
+  const tour = tourData.find(tour => tour.id === id)
+
+  // destructure properties from tour object
+  const {photo, title, desc, price, address, reviews, city, distance, maxGroupSize} = tour
+
+  // rating
+  const {totalRating, avgRating} = calculateRating(reviews)
+
   return (
-    <div>
-        TourDetail
-    </div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='8'>
+              <div className="tour__content">
+                <img src={photo} alt="" />
+                <div className="tour__info">
+                  <h2>{title}</h2>
+                  <div className="d-flex align-items-center gap-5">
+                    <span className="tour__rating d-flex align-items-center gap-1">
+                        {/* check rating */}
+                        <i className="ri-star-fill" style={{'color':'var(--secondary-color)'}}></i> {avgRating === 0 ? null : avgRating}
+                        {totalRating === 0 ? "Not rated" : <span>({reviews?.length})</span>}
+                    </span>
+                    <span>
+                      <i className="ri-map-pin-user-fill"></i> {address}
+                    </span>
+                  </div>
+                  <div className="tour__extra-details">
+                    <span> <i className="ri-map-pin-2-line"></i> {city} </span>
+                    <span> <i className="ri-money-dollar-circle-line"></i> ${price} /per person </span>
+                    <span> <i className="ri-group-line"></i> {maxGroupSize} </span>
+                  </div>
+                  <h5>Description</h5>
+                  <p>{desc}</p>
+                </div>
+                {/*  */}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 

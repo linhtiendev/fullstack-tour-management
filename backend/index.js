4e1aadaf5178ec21fc0a9a842c0536f32e2fa6db
linhtiendev <lelinhtien0707@gmail.com>
2024-01-26 14:02:17 +0700

    feat: API update, delete, get single, get all user

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
 
 dotenv.config();
 const app = express();
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use("/tours", tourRoute);
+app.use("/users", userRoute);
 
 app.listen(port, () => {
     connect();

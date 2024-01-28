71807384163f1cf284ce92985e650e87d75d0971
linhtiendev <lelinhtien0707@gmail.com>
2024-01-28 17:28:51 +0700

    feat: API review tour

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -68,7 +68,7 @@ export const getSingleTour = async (req, res) => {
     const id = req.params.id;
 
     try {
-        const tour = await Tour.findById(id);
+        const tour = await Tour.findById(id).populate("reviews");
         res.status(200).json({
             success: true,
             message: "Successful",
@@ -89,6 +89,7 @@ export const getAllTour = async (req, res) => {
 
     try {
         const tours = await Tour.find({})
+            .populate("reviews")
             .skip(page * 8) //pagination (8)
             .limit(8);
 
@@ -119,7 +120,7 @@ export const getTourBySearch = async (req, res) => {
             city,
             distance: { $gte: distance },
             maxGroupSize: { $gte: maxGroupSize },
-        });
+        }).populate("reviews");
         res.status(200).json({
             success: true,
             message: "Successful",
@@ -137,7 +138,9 @@ export const getTourBySearch = async (req, res) => {
 export const getFeaturedTour = async (req, res) => {
     try {
         // tìm featured =  true
-        const tours = await Tour.find({ featured: true }).limit(8);
+        const tours = await Tour.find({ featured: true })
+            .populate("reviews")
+            .limit(8);
 
         res.status(200).json({
             success: true,

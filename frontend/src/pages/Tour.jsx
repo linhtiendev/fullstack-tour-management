b57ccc1da4c83aca0588831096cc55bea165a593
linhtiendev <lelinhtien0707@gmail.com>
2024-02-20 10:32:33 +0700

    feat: pagination all tour

--- frontend/src/pages/Tour.jsx
+++ frontend/src/pages/Tour.jsx
@@ -2,22 +2,28 @@ import React, {useState, useEffect} from 'react'
 import '../styles/tour.css'
 
 import CommonSection from '../shared/CommonSection'
-import tourData from '../assets/data/tours'
 import TourCard from '../shared/TourCard'
 import SearchBar from '../shared/SearchBar'
 import NewsLetter from '../shared/Newsletter'
 
 import { Container, Row, Col } from 'reactstrap'
 
+import useFetch from '../hooks/useFetch'
+import { BASE_URL } from '../utils/config'
+
 const Tours = () => {
 
   const [pageCount, setPageCount] = useState(0)
   const [page, setPage] = useState(0)
 
+  const {data: tours, loading, error} = useFetch(`${BASE_URL}/tours?page=${page}`)
+  const {data: tourCount} = useFetch(`${BASE_URL}/tours/search/getTourCounts`)
+
   useEffect(() => {
-    const pages = Math.ceil(5 / 4) // later we will use back end data count
+    const pages = Math.ceil(tourCount / 8)
     setPageCount(pages)
-  }, [page])
+    window.scrollTo(0, 0) // scroll up khi chuyen trang
+  }, [page, tourCount, tours])
 
   return (
     <>
@@ -31,10 +37,13 @@ const Tours = () => {
       </section>
       <section className='pt-0'>
         <Container>
-          <Row>
+          {loading && <h4 className='text-center pt-5'>Loading...</h4>}
+          {error && <h4 className='text-center pt-5'>{error}</h4>}
+          {
+            !loading && !error && <Row>
             {
-              tourData?.map(tour => ( 
-                <Col lg='3' className='mb-4' key={tour.id}>
+              tours?.map(tour => ( 
+                <Col lg='3' className='mb-4' key={tour._id}>
                   <TourCard tour={tour}/>
                 </Col>
             ))}
@@ -53,6 +62,7 @@ const Tours = () => {
               </div>
             </Col>
           </Row>
+          }
         </Container>
       </section>
       <NewsLetter />

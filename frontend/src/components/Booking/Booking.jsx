00b85cba14a3240a95be85b66c04309f1b69840b
linhtiendev <lelinhtien0707@gmail.com>
2024-02-21 14:50:23 +0700

    feat: fetch api tour booking

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,19 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
 import './booking.css'
 
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap'
 
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+
+import { BASE_URL } from '../../utils/config'
 
 const Booking = ({tour, avgRating}) => {
 
-    const {price, reviews} = tour
-
+    const {price, reviews, title} = tour
     const navigate = useNavigate() 
 
-    const [credentials, setCredentials] = useState({
-        userId: '01', //later it will be dynamic
-        userEmail: 'example@gmail.com', 
+    const { user } = useContext(AuthContext)
+
+    const [ booking, setBooking ] = useState({
+        userId: user && user._id,
+        userEmail: user && user.email,
+        tourName: title,
         fullName: '',
         phone: '',
         guestSize: 1,
@@ -21,23 +26,47 @@ const Booking = ({tour, avgRating}) => {
     })
 
     const handleChange = (e) => {
-        setCredentials(prev => ({...prev, [e.target.id]:e.target.value}),)
+        setBooking(prev => ({...prev, [e.target.id]:e.target.value}),)
     }
 
     // hàm phí dịch vụ
     const serviceFee = 10
     // hàm tính tổng
-    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee)
+    const totalAmount = Number(price) * Number(booking.guestSize) + Number(serviceFee)
 
 
     // send data to the server
-    const handleClick = (e) => {
+    const handleClick = async (e) => {
         e.preventDefault();
-        navigate("/thank-you")
+        console.log(booking);
+        try {
+            if (!user || user === undefined || user === null) {
+                return alert('Please sign in')
+            }
+
+            const res = await fetch(`${BASE_URL}/booking`, 
+                {
+                    method: 'post',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    credentials: 'include',
+                    body: JSON.stringify(booking)
+                }
+            )
+
+            const result = await res.json()
+            if (!res.ok) {
+                return alert(result.message)
+            }
+            navigate("/thank-you")
+
+        } catch (err) {
+            alert(err.message)
+        }
     }
 
   return (
-
     <div className="booking">
         <div className="booking__top d-flex align-items-center 
         justify-content-between">

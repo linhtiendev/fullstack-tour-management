9066f3358ed0416253baff6cafbfc9b444c5810c
linhtiendev <lelinhtien0707@gmail.com>
2024-02-21 12:52:05 +0700

    feat: fetch api register and login

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useContext} from 'react'
 // sử dụng thư viện reactstrap
 import { Container, Row, Button } from 'reactstrap'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, useNavigate } from 'react-router-dom'
 
 import logo from '../../assets/images/logo.png'
 import './header.css'
+
+import { AuthContext } from '../../context/AuthContext'
 
 // Navigation header
 const nav__links=[
@@ -26,6 +28,13 @@ const Header = () => {
 
   // hàm scroll header
   const headerRef = useRef(null)
+  const navigate = useNavigate()
+  const { user, dispatch } = useContext(AuthContext)
+
+  const logout = () => {
+    dispatch({type: 'LOGOUT'})
+    navigate('/')
+  }
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -75,12 +84,20 @@ const Header = () => {
 
             <div className="nav__right d-flex align-items-center gap-4">
               <div className="nav__btn d-flex align-items-center gap-4">
-                  <Button className='btn secondary__btn'>
-                    <Link to='/login'>Login</Link>
-                  </Button>
-                  <Button className='btn primary__btn'>
-                    <Link to='/register'>Register</Link>
-                  </Button>
+                {
+                  user? <>
+                    <h5 className="mb-0">{user.username}</h5>
+                    <Button className="btn btn-dark" onClick={logout}>Logout</Button>
+                  </> : <>
+                    <Button className='btn secondary__btn'>
+                      <Link to='/login'>Login</Link>
+                    </Button>
+                    <Button className='btn primary__btn'>
+                      <Link to='/register'>Register</Link>
+                    </Button>
+                  </>
+                }
+                  
               </div>
               {/*========= mobile menu =========*/}
               <span className="mobile__menu">

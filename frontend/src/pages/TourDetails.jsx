70c03c049bd2e40a9ff7c3ee49dd08ffbb669af9
linhtiendev <lelinhtien0707@gmail.com>
2024-02-21 14:49:52 +0700

    feat: fetch api review

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, useContext } from 'react'
 import '../styles/tour-details.css'
 
 import avatar from '../assets/images/avatar.jpg'
@@ -14,11 +14,14 @@ import NewsLetter from '../shared/Newsletter'
 import useFetch from './../hooks/useFetch'
 import {BASE_URL} from './../utils/config'
 
+import { AuthContext } from './../context/AuthContext'
+
 const TourDetails = () => {
 
   const {id} = useParams()
   const reviewMsgRef = useRef('')
   const [tourRating, setTourRating] = useState(null)
+  const {user} = useContext(AuthContext)
 
   // fetch data from database
   const {data: tour, loading, error} = useFetch(`${BASE_URL}/tours/${id}`)
@@ -33,12 +36,40 @@ const TourDetails = () => {
   const options = { day: 'numeric', month: 'long', year: 'numeric'}
 
   //submit request to the server
-  const submitHandler = e => {
+  const submitHandler =  async e => {
     e.preventDefault()
     const reviewText = reviewMsgRef.current.value
-    // alert(`${reviewText}, ${tourRating}`)
+  
+    try {
+      if (!user || user === undefined || user === null) {
+        alert('Please sign in')
+      }
 
-    //later will call our api
+      const reviewObj = {
+        username: user?.username,
+        reviewText,
+        rating: tourRating
+      }
+
+      const res = await fetch(`${BASE_URL}/review/${id}`,
+        {
+          method: 'post',
+          headers: {
+            'content-type': 'application/json'
+          },
+          credentials: 'include',
+          body: JSON.stringify(reviewObj)
+        }
+      )
+
+      const result = await res.json()
+      if (!res.ok) {
+        return alert(result.message)
+      }
+      alert(result.message)
+    } catch (err) {
+      alert(err.message)
+    }
   }
 
   useEffect(() => {
@@ -114,18 +145,18 @@ const TourDetails = () => {
                             <div className='d-flex align-items-center 
                             justify-content-between'>
                               <div>
-                                <h5>Muhib</h5>
+                                <h5>{review.username}</h5>
                                 <p>
-                                  {new Date("01-01-2024").toLocaleDateString(
+                                  {new Date(review.createdAt).toLocaleDateString(
                                     "en-US", options
                                   )}
                                 </p>
                               </div>
                               <span className='d-flex align-items-center'>
-                                  5 <i className="ri-star-s-fill"></i> 
+                                  {review.rating} <i className="ri-star-s-fill"></i> 
                               </span>
                             </div>
-                            <h6>Amazing tour</h6>
+                            <h6>{review.reviewText}</h6>
                           </div>
                         </div>
                       ))
